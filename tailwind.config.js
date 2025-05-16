69db06e1d256a34e9088330021bacaefe7e7c143
Samuel <samuelsuzigan@gmail.com>
2025-05-16 12:02:22 -0300

    vaidenovo

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        verdeMar: '#00cba9',
+      },
+      fontFamily: {
+        titulo: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
